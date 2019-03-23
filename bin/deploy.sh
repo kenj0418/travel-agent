8cea0166c9c5ef760cb67e305c1a97419b99ff67
Ken Johnson <ken@Kens-iMac.local>
2019-03-23 18:10:20 -0500

    Done playing around with photos for now

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,5 +6,5 @@ aws s3 cp ../alexa-travel-agent.zip s3://kenj0418-alexa-travel-agent/deployables
 
 aws lambda update-function-code --function-name alexa-travel-agent-TravelAgentLambda-16TP3I0ZKN1W3 --s3-bucket kenj0418-alexa-travel-agent --s3-key "deployables/alexa-travel-agent-0.zip"
 
-#todo restoring dev dependancies to avoid being annoying (disable when move to a CI build script)
-#npm i
+#restoring dev dependancies to avoid being annoying (disable when move to a CI build script)
+npm i
--- test/unit/alexa/travelAgent.test.js
+++ test/unit/alexa/travelAgent.test.js
@@ -9,29 +9,28 @@ const randomNumber = require("random-number").generator({
 const randomString = require("random-string")
 const flightServices = require("../../../lib/services/flightServices")
 
-xdescribe("travelAgent alexa handler", () => {
-  let alexa
+describe("travelAgent alexa handler", () => {
+  let virtualAlexa
   let mockGetRoutes
 
   beforeEach(() => {
     mockGetRoutes = sinon.stub(flightServices, "getRoutesForPairAndDate")
 
-    alexa = vax.VirtualAlexa.Builder()
+    virtualAlexa = vax.VirtualAlexa.Builder()
       .applicationID("amzn1.ask.skill.f000006c-c911-492b-bdf8-df0499442b9c")
       .handler("lib/alexa/travelAgent.handler")
       .interactionModelFile("test/model/travelAgent.json")
       .create()
+
+    virtualAlexa.dynamoDB().mock()
   })
 
   afterEach(() => {
     mockGetRoutes.restore()
   })
 
-  xit("need to figure out how to mock out the database persistence") //todo
-  //todo also update skill info here from other project when its ready
-
-  it("Startup, Intro", async () => {
-    let reply = await alexa.launch()
+  it("Startup, New User", async () => {
+    let reply = await virtualAlexa.launch()
     let replySSML = reply.response.outputSpeech.ssml
     expect(replySSML).to.include.string("Welcome to Travel Agent")
     expect(replySSML).to.include.string(
@@ -43,12 +42,24 @@ xdescribe("travelAgent alexa handler", () => {
     expect(replySSML).to.include.string("What would you like to know?")
   })
 
+  xit("Photo of Chicago", async () => {
+    const cityInfoRequest = virtualAlexa
+      .request()
+      .intent("CityInfoIntent")
+      .slot("city", "Chicago")
+
+    const response = await cityInfoRequest.send()
+    console.log("response", JSON.stringify(response))
+    let replySSML = response.outputSpeech.ssml
+    expect(replySSML).to.equal("todo something")
+  })
+
   xit("Startup, Returning User")
 
   it("Help", async () => {
-    await alexa.launch()
+    await virtualAlexa.launch()
 
-    const helpReply = await alexa.utter("Help")
+    const helpReply = await virtualAlexa.utter("Help")
     let replySSML = helpReply.response.outputSpeech.ssml
     expect(replySSML).to.include("You can say")
     expect(replySSML).to.include("How much is a flight to Chicago")
@@ -58,25 +69,25 @@ xdescribe("travelAgent alexa handler", () => {
   })
 
   it("Stop", async () => {
-    await alexa.launch()
-    const stopReply = await alexa.utter("Stop")
+    await virtualAlexa.launch()
+    const stopReply = await virtualAlexa.utter("Stop")
     let replySSML = stopReply.response.outputSpeech.ssml
     expect(replySSML).to.include("Goodbye")
   })
 
   it("Cancel", async () => {
-    await alexa.launch()
-    const cancelReply = await alexa.utter("Cancel")
+    await virtualAlexa.launch()
+    const cancelReply = await virtualAlexa.utter("Cancel")
     let replySSML = cancelReply.response.outputSpeech.ssml
     expect(replySSML).to.include("Goodbye")
   })
 
-  it("Both cities and date specified", async () => {})
+  xit("Both cities and date specified", async () => {})
 
   xit("No routes", async function() {
     mockGetRoutes.returns([])
 
-    let reply = await alexa.launch()
+    let reply = await virtualAlexa.launch()
     expect(reply.response.outputSpeech.ssml).to.include.string(
       "Sorry, I could not find any flights for you."
     )
@@ -86,7 +97,7 @@ xdescribe("travelAgent alexa handler", () => {
     const testPrice = randomNumber()
     mockGetRoutes.returns([{ MinPrice: testPrice }])
 
-    let reply = await alexa.launch()
+    let reply = await virtualAlexa.launch()
     expect(reply.response.outputSpeech.ssml).to.include.string(
       `I found a route that cost ${testPrice} dollars`
     )
@@ -95,7 +106,7 @@ xdescribe("travelAgent alexa handler", () => {
   xit("Error looking up routes", async function() {
     mockGetRoutes.throws(randomString())
 
-    let reply = await alexa.launch()
+    let reply = await virtualAlexa.launch()
     expect(reply.response.outputSpeech.ssml).to.include.string(
       "Sorry, there was a problem looking up flights."
     )
