037fa62136118b80b186d4095752d33eb897317b
Ken Johnson <ken@Kens-iMac.local>
2019-03-22 23:35:57 -0500

    Playing around with image cards and directives for long running requests

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,5 +6,5 @@ aws s3 cp ../alexa-travel-agent.zip s3://kenj0418-alexa-travel-agent/deployables
 
 aws lambda update-function-code --function-name alexa-travel-agent-TravelAgentLambda-16TP3I0ZKN1W3 --s3-bucket kenj0418-alexa-travel-agent --s3-key "deployables/alexa-travel-agent-0.zip"
 
-#restoring dev dependancies to avoid being annoying (disable when move to a CI build script)
-npm i
+#todo restoring dev dependancies to avoid being annoying (disable when move to a CI build script)
+#npm i
